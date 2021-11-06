4339262c639de0c11b018a04001059d88bc9106d
Jeffrey Kirchner <jkirchner@gmail.com>
2021-11-05 20:31:49 -0700

    increase message size

--- main/models/mass_email.py
+++ main/models/mass_email.py
@@ -16,7 +16,7 @@ class MassEmail(models.Model):
     app = models.ForeignKey(User, on_delete=models.CASCADE, related_name="app_name")
 
     message_subject = models.CharField(max_length = 300, default="Message Subject", verbose_name="Message Subject")    #message subject
-    message_text = models.CharField(max_length = 10000, default="Message Text", verbose_name="Message Text")           #message text
+    message_text = models.CharField(max_length = 100000, default="Message Text", verbose_name="Message Text")           #message text
     user_list = models.JSONField(verbose_name="User List")                                                             #list of users and template variables
     email_result = models.JSONField(verbose_name="Email Result", default = default_email_result)                       #number of emials successfully sent
     memo = models.CharField(max_length = 300, default="", verbose_name="Memo")                                         #note about message's purpose
