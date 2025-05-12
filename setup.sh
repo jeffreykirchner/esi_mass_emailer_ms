10aceeffcc3cd36fb617aeb630c6f09e2fe3c8a2
Jeffrey Kirchner <jkirchner@gmail.com>
2025-05-12 16:56:42 +0000

    chore: Update requirements and add setup commands for site parameters and superuser

---
+++ setup.sh
@@ -0,0 +1,19 @@
+echo "Setup ESI Mass Emailer"
+sudo service postgresql restart
+sudo service redis-server start
+echo "Drop mass_emailer db: enter db password"
+dropdb mass_emailer -U dbadmin -h localhost -i -p 5432
+echo "Create database: enter db password"
+createdb -h localhost -p 5432 -U dbadmin -O dbadmin mass_emailer
+echo "Restore database? (y/n)"
+read restore
+if [ "$restore" = "y" ]; then
+    echo "Restore database: enter db password"
+    pg_restore -v --no-owner --role=dbowner --host=localhost --port=5432 --username=dbadmin --dbname=mass_emailer database_dumps/mass_emailer.sql
+else
+    python manage.py migrate
+    echo "Create Super User:"
+    python manage.py setup_superuser_with_profile
+    python manage.py setup_site_parameters
+fi
+echo "Setup complete."
